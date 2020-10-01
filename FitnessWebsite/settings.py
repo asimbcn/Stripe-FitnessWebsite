90be3aa6a7dcdc40af0ae61e56fbc2240acd2508
Asim Raj Pandey <asimbcn@gmail.com>
2020-10-01 14:12:13 +0545

    Directory management

--- FitnessWebsite/settings.py
+++ FitnessWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rv8#^jg#)aos(&i%#dpys9@4*+_#kuhe%4qfb&frh*z53tr1+c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ["asimbcn-fitness.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
